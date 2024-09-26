a35a5a8d36233b5d82989502a1225aa3bb1f58f1
dudoserdkr <dudoserdkr@gmail.com>
2024-09-26 19:13:11 +0300

    +HW6

--- HW_6_C/t06_13.cpp
+++ HW_6_C/t06_13.cpp
@@ -1,3 +1,21 @@
-//
-// Created by Ivan Tyulpa on 26.09.2024.
-//
+#include <iostream>
+#include <stdint.h>
+#include <bitset>
+
+using namespace std;
+
+int main() {
+    unsigned int n;
+    int counter = 0;
+
+    cin >> n;
+    cout << bitset<32>(n) << endl;
+
+    while(n) {
+        if ((n & 1) == 1) {
+            counter++;
+        }
+        n >>= 1;
+    }
+    cout << counter;
+}
--- HW_6_C/t06_22.cpp
+++ HW_6_C/t06_22.cpp
@@ -1,3 +1,20 @@
-//
-// Created by Ivan Tyulpa on 26.09.2024.
-//
+#include <iostream>
+#include <bitset>
+#include <stdint.h>
+
+using namespace std;
+
+long int foo(long int n, unsigned k) {
+    long int a = ~0;
+    a = a << k;
+    return n | a;
+}
+
+int main() {
+    long int n;
+    unsigned k;
+
+    cin >> n >> k;
+    cout << bitset<64>(n) << endl;
+    cout << bitset<64>(foo(n, k)) << endl;
+}
